a221ba59e0759f14d398587054a33a703d6feded
Rsujai <129419852+Rsujai@users.noreply.github.com>
2023-10-18 13:58:11 +0530

    Update applofstack.c

--- applofstack.c
+++ applofstack.c
@@ -1,93 +1,102 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <ctype.h>
-#define MAX_SIZE 100
-typedef struct {
-    char data[MAX_SIZE];
-    int top;
-} Stack;
+#include <stdlib.h>
+#include <string.h>
 
-void initialize(Stack* stack) {
-    stack->top = -1;
-}
-void push(Stack* stack, char value) {
-    stack->data[++stack->top] = value;
-}
-char pop(Stack* stack) {
-    return stack->data[stack->top--];
-}
-bool isEmpty(Stack* stack) {
-    return stack->top == -1;
-}
-int evaluatePostfixExpression(const char* expression) {
-    Stack stack;
-    initialize(&stack);
+#define MAX_EXPR_SIZE 100
 
-    for (int i = 0; expression[i] != '\0'; i++) {
-        if (isdigit(expression[i])) {
-            push(&stack, expression[i]);
-        } else if (expression[i] == ' ') {
-            continue;  // Ignore spaces
-        } else {
-            int operand2 = pop(&stack) - '0';
-            int operand1 = pop(&stack) - '0';
-            int result;
-
-            switch (expression[i]) {
-                case '+':
-                    result = operand1 + operand2;
-                    break;
-                case '-':
-                    result = operand1 - operand2;
-                    break;
-                case '*':
-                    result = operand1 * operand2;
-                    break;
-                case '/':
-                    result = operand1 / operand2;
-                    break;
-            }
-            push(&stack, result + '0');
-        }
-    }
-    return pop(&stack) - '0';
+// Function to return precedence of operators
+int precedence(char operator)
+{
+switch (operator) {
+case '+':
+case '-':
+return 1;
+case '*':
+case '/':
+return 2;
+case '^':
+return 3;
+default:
+return -1;
 }
-bool isBracketMatch(const char* expression) {
-    Stack stack;
-    initialize(&stack);
-
-    for (int i = 0; expression[i] != '\0'; i++) {
-        if (expression[i] == '(') {
-            push(&stack, '(');
-        } else if (expression[i] == ')') {
-            if (isEmpty(&stack)) {
-                return false;
-            }
-            pop(&stack);
-        }
-    }
-
-    return isEmpty(&stack);
 }
 
-int main() {
-    char postfixExpression[MAX_SIZE];
-    printf("Enter a postfix expression: ");
-    scanf("%[^\n]", postfixExpression);
-    getchar(); // Consume the newline character
+// Function to check if the scanned character
+// is an operator
+int isOperator(char ch)
+{
+return (ch == '+' || ch == '-' || ch == '*' || ch == '/'
+|| ch == '^');
+}
 
-    int result = evaluatePostfixExpression(postfixExpression);
-    printf("Postfix expression result: %d\n", result);
+// Main functio to convert infix expression
+// to postfix expression
+char* infixToPostfix(char* infix)
+{
+int i, j;
+int len = strlen(infix);
+char* postfix = (char*)malloc(sizeof(char) * (len + 2));
+char stack[MAX_EXPR_SIZE];
+int top = -1;
 
-    char bracketExpression[MAX_SIZE];
-    printf("Enter an expression with brackets: ");
-    scanf("%[^\n]", bracketExpression);
-    getchar(); // Consume the newline character
+for (i = 0, j = 0; i < len; i++) {
+if (infix[i] == ' ' || infix[i] == '\t')
+continue;
 
-    if (isBracketMatch(bracketExpression)) {
-        printf("Brackets are matched.\n");
-    } else {
-        printf("Brackets are not matched.\n");
-    }
-    return 0;
+// If the scanned character is operand
+// add it to the postfix expression
+if (isalnum(infix[i])) {
+postfix[j++] = infix[i];
+}
+
+// if the scanned character is '('
+// push it in the stack
+else if (infix[i] == '(') {
+stack[++top] = infix[i];
+}
+
+// if the scanned character is ')'
+// pop the stack and add it to the
+// output string until empty or '(' found
+else if (infix[i] == ')') {
+while (top > -1 && stack[top] != '(')
+postfix[j++] = stack[top--];
+if (top > -1 && stack[top] != '(')
+return "Invalid Expression";
+else
+top--;
+}
+
+// If the scanned character is an operator
+// push it in the stack
+else if (isOperator(infix[i])) {
+while (top > -1
+&& precedence(stack[top])
+>= precedence(infix[i]))
+postfix[j++] = stack[top--];
+stack[++top] = infix[i];
+}
+}
+
+// Pop all remaining elements from the stack
+while (top > -1) {
+if (stack[top] == '(') {
+return "Invalid Expression";
+}
+postfix[j++] = stack[top--];
+}
+postfix[j] = '\0';
+return postfix;
+}
+
+// Driver code
+int main()
+{
+char infix[MAX_EXPR_SIZE] = "3+4*2";
+
+// Function call
+char* postfix = infixToPostfix(infix);
+printf("%s\n", postfix);
+free(postfix);
+return 0;
 }
